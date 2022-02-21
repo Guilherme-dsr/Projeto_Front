70530174460310083cef15c87a3c969d9b5f6477
Guilherme <guilhermesilva.dsr@gmail.com>
2022-02-20 21:49:12 -0300

    Atualizando o Formulário de cadastro

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-md">
+  <a class="navbar-brand" href="#">CRUD - NODE AND VUE</a>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">MENU</a>
+      </li>
+    </ul>
+  </div>
+  </div>
+</nav>
+</template>
+
+<script>
+  export default{
+    name: 'Header'
+  }
+</script>
+
+<style>
+</style>
